bdaceb9cdf0dd06c1844686b7431f126c8895b31
Christopher Chaison <cchaison0@gmail.com>
2020-03-19 21:31:21 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,3 +4,9 @@ require 'open-uri'
 doc = Nokogiri::HTML(open("https://flatironschool.com/"))
 
 doc.css(".headline-260IBN").text.strip
+
+courses = doc.css("#3c239880-48cf-5ce7-84d0-578546d87586 .inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC")
+
+courses.each do |course|
+  puts course.text.strip
+end
